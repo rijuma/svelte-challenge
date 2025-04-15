9eca860da2da556d881e6a2bf62b7272d66986db
Marcos <rijuma@users.noreply.github.com>
2025-04-15 19:54:09 -0300

    Added navigation-hover on vanilla

---
+++ vanilla-js/1-navigation-hover/main.js
@@ -0,0 +1,43 @@
+// This calls the browser's transition API before and after the DOM update within the update function parameter.
+async function transition(update) {
+  if (document?.startViewTransition) {
+    const transition = document.startViewTransition(() => update())
+    await transition.finished
+  } else {
+    update()
+  }
+}
+
+async function handleMenuClick(event) {
+  const wrapperLi = event.target?.parentNode
+
+  // Skip action if the active element is the active one.
+  if (!wrapperLi || wrapperLi.classList.contains('active')) return
+
+  await transition(() => {
+    // Remove the active
+    const activeLi = document.querySelector('.main-nav li.active')
+    activeLi?.classList.remove('active')
+
+    wrapperLi.classList.add('active')
+  })
+}
+
+// --
+
+const controller = new AbortController()
+const signal = controller.signal
+
+function init() {
+  const links = document.querySelectorAll('.main-nav li')
+
+  links.forEach(link => {
+    link.addEventListener('click', handleMenuClick, { signal })
+  })
+}
+
+function cleanup() {
+  controller.abort() // Event binding cleanup.
+}
+
+window.addEventListener('load', init)
