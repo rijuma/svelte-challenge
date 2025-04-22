d1e4760e5692a551e74f2e2992fe4193ee18c9ef
Marcos <rijuma@users.noreply.github.com>
2025-04-22 12:29:04 -0300

    Updated task 2 responsiveness

--- src/routes/(solutions)/2-gradient/+page.svelte
+++ src/routes/(solutions)/2-gradient/+page.svelte
@@ -1,8 +1,8 @@
 <a
   href="https://bing.com"
-  class="flex flex-col gap-8 bg-white text-zinc-600 shadow-lg no-underline transition-all duration-500 relative text-center text-5xl rounded-[9rem] hover:scale-105 hover:shadow-2xl"
+  class="[--size:min(26.75rem,70vw)] grid place-items-center w-(--size) h-(--size) bg-white text-zinc-600 shadow-lg no-underline relative text-center text-5xl rounded-[32%] hover:scale-105 hover:shadow-2xl transition-all"
 >
-  <figure class="flex flex-col gap-8 w-80 h-80 aspect-square p-12 box-content m-4">
+  <figure class="flex flex-col gap-[16%] max-w-1/2 absolute inset-0 m-auto aspect-square">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xml:space="preserve"
@@ -13,13 +13,13 @@
       text-rendering="geometricPrecision"
       viewBox="0 0 19000 19000"
       aria-labelledby="bing-logo-label"
-      class="relative left-4"
+      class="w-full left-[6%] top-[3%]"
     >
       <path
         fill="currentColor"
         d="M3125.7 392.54 6774.63 1677.9l12 12800.2 5087.15-2965-2482.23-1161.23-1592.8-3956.11 8075.56 2828.42-6.61 4144.96-9094.12 5238.33-3636.03-2023.34z"
       />
     </svg>
-    <figcaption id="bing-logo-label" class="text-5xl">Bing</figcaption>
+    <figcaption id="bing-logo-label" class="text-[min(2.75rem,7vw)]">Bing</figcaption>
   </figure>
 </a>
